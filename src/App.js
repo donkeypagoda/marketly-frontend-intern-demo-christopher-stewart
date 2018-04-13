135458888d5799e052feabf61c3fb43a6eada4bb
donkeypagoda <christopherallenstewart@gmail.com>
2018-04-13 11:52:06 -0700

    api is call working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      searchResult: []
+    }
+    this.submitSearch = this.submitSearch.bind(this)
+  }
+
+  async submitSearch(e){
+    e.preventDefault()
+    console.log(e.target[0].value)
+    const response = await fetch(`http://www.recipepuppy.com/api/?q=${e.target[0].value}`)
+    const results = await response.json()
+    console.log(results.results)
+  }
+
   render() {
     return (
       <div className="App">
-      Test?
+        <div className="searchForm">
+        <form onSubmit={this.submitSearch}>
+          <label>Search for Meals Here!</label>
+            <input type="text" />
+          <button type="submit" className="submitButton">Search Meals</button>
+        </form>
+        </div>
       </div>
     );
   }
--- src/components/searchBox.js
+++ src/components/searchBox.js
@@ -1,18 +1,1 @@
-import React, { Component } from 'react';
 
-class SearchBox extends Component{
-
-
-  render(){
-    return(
-      <div>
-        <form className="searchform" onSubmit={()=> this.submitSearch}>
-          <input type="text" />
-        </form>
-      </div>
-    )
-  }
-}
-
-
-export default SearchBox
